fe8d7f47341d2e2860ac581dae119f4e6c086654
fiezz <nexfiez@gmail.com>
2026-08-10 13:19:41 +0800

    Revert "Initial commit: JobTrack with Docker support"
    This reverts commit b7944f6a09dbb3e75214fa4b66494107e6cb6f3b.

--- resources/js/Components/ApplicationLogo.jsx
+++
@@ -1,7 +0,0 @@
-export default function ApplicationLogo({ className = '', ...props }) {
-    return (
-        <span {...props} className={`font-bold text-xl tracking-tight ${className}`}>
-            JobTracker
-        </span>
-    );
-}
